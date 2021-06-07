9377d6d837434f30d2a11cf69fc00c2e32986bcf
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-07 14:56:27 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,47 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
+// Importing Custom Modules - Mudasir Ali
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+import { BlocksModule } from './blocks/blocks.module';
+import { AdminModule } from './admin/admin.module';
+import { EmployeeModule } from './employee/employee.module';
+import { ClientModule } from './client/client.module';
+
 registerLocaleData(en);
 
+// Modules Variables - Mudasir Ali
+
+const CustomModules = [
+  CoreModule,
+  BlocksModule,
+  FeaturesModule,
+  AdminModule,
+  ClientModule,
+  EmployeeModule,
+];
+
+const AdminModules = []
+
+const ClientModules = []
+
+const EmployeeModules = []
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    CustomModules,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

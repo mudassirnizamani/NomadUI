0bf23518f3dbde20ad98b5f94044841fe5d31a0f
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-09 14:49:03 -0700

    Made Some Changes in Features Module

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [AuthModule],
 })
-export class FeaturesModule { }
+export class FeaturesModule {}

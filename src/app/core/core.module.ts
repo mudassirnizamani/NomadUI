043527d2cbc37661181746c8af948a256a1b5795
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-15 09:48:39 -0700

    Created UserService in Core Module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 // Importing Services - Mudasir Ali
 import { SignoutService } from './services/signout/signout.service';
+import { UserService } from './services/user/user.service';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
-  providers: [SignoutService],
+  providers: [SignoutService, UserService],
 })
 export class CoreModule {}
---
+++ src/app/core/services/user/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getAuthenticatedUser() {
+    var tokenHeader = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    });
+    return this.http.get(
+      environment.APIBaseUrl + environment.AuthUrls.GetUser,
+      { headers: tokenHeader }
+    );
+  }
+
+  getAllUsers() {
+    return this.http.get(environment.APIBaseUrl + environment.User.GetAllUsers);
+  }
+
+  gerUserByUserName(username: string) {
+    return this.http.get(
+      `${environment.APIBaseUrl}${environment.User.GetUserByUserName}${username}`
+    );
+  }
+}

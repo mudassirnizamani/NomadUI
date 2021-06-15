356b52604372172277c1b0b822992f571dac9d70
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-15 09:53:20 -0700

    Added Spinner in SideBar User Profile Component

--- src/app/shared/components/sidebar-user-profile/sidebar-user-profile.component.ts
+++ src/app/shared/components/sidebar-user-profile/sidebar-user-profile.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user/user.service';
 
 @Component({
   selector: 'app-sidebar-user-profile',
   templateUrl: './sidebar-user-profile.component.html',
-  styleUrls: ['./sidebar-user-profile.component.scss']
+  styleUrls: ['./sidebar-user-profile.component.scss'],
 })
 export class SidebarUserProfileComponent implements OnInit {
+  isLoading: boolean = false;
+  userData: any;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.userService.getAuthenticatedUser().subscribe(
+      (res: any) => {
+        if (res.succeeded) {
+          this.userData = res.user;
+          this.isLoading = false;
+        } else if (!res.succeeded && res.code == 'ServerError') {
+          this.userData.firstName = 'Error';
+        }
+      },
+      (error: any) => {
+        this.userData.firstName = 'Error';
+        this.isLoading = false;
+      }
+    );
   }
-
 }

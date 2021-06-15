49a2a15097010779585a678f2ccdb57d401049e8
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-15 09:44:49 -0700

    Added Loading Spinner Component in APP

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 // Importing Components
 import { ThemeToggleSwitchComponent } from './components/theme-toggle-switch/theme-toggle-switch.component';
 import { SidebarUserProfileComponent } from './components/sidebar-user-profile/sidebar-user-profile.component';
-
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 // Importing Ant Design Modules - Mudasir Ali
-
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 @NgModule({
-  declarations: [ThemeToggleSwitchComponent, SidebarUserProfileComponent],
-  imports: [],
-  exports: [ThemeToggleSwitchComponent, SidebarUserProfileComponent]
+  declarations: [
+    ThemeToggleSwitchComponent,
+    SidebarUserProfileComponent,
+    SpinnerComponent,
+  ],
+  imports: [NzSpinModule],
+  exports: [
+    ThemeToggleSwitchComponent,
+    SidebarUserProfileComponent,
+    SpinnerComponent,
+  ],
 })
 export class SharedModule {}

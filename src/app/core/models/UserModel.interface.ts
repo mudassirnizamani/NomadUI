fc71283b0043061c1f78a0ee040b5b78f74b489b
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-06-15 09:47:10 -0700

    Added UserModel in Code Module

---
+++ src/app/core/models/UserModel.interface.ts
@@ -0,0 +1,20 @@
+export interface UserModel {
+  firstName: string;
+  lastName: string;
+  accessFailedCount: number;
+  email: string;
+  emailConfirmed: boolean;
+  id: string;
+  lockoutEnabled: string;
+  lockoutEnd: string;
+  normalizedEmail: string;
+  normalizedUserName: string;
+  phoneNumber: string;
+  phoneNumberConfirmed: boolean;
+  profilePic: string;
+  twoFactorEnabled: boolean;
+  userName: string;
+  isActive: boolean;
+  lastLoginIp: string;
+  createdAt: string;
+}

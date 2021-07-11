ff802b061d7a359908280246a0b30aac324a1dde
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-11 16:37:54 -0700

    Added ClientSignup Model

--- src/app/features/auth/models/ClientSignup.interface.ts
+++ src/app/features/auth/models/ClientSignup.interface.ts
@@ -1,3 +1,9 @@
 export interface ClientModel {
-    
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  profilePic: string;
 }

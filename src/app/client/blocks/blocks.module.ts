8e895ba41550b26055937700ed73f74a754b7420
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-13 09:38:46 -0700

    Made changes in Client Module

--- src/app/client/blocks/blocks.module.ts
+++ src/app/client/blocks/blocks.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IndexLayoutComponent } from './layouts/index-layout/index-layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  declarations: [IndexLayoutComponent],
-  imports: [RouterModule],
+  declarations: [IndexLayoutComponent, SidebarComponent, TopbarComponent],
+  imports: [RouterModule, SharedModule, NzDropDownModule, NzIconModule],
 })
 export class BlocksModule {}

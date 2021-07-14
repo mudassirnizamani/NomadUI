0790827ecd2f4106be7e54465ad0b99ceee098dd
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-14 16:10:49 -0700

    Added new Modules in Client's Feature Module

--- src/app/client/features/features.module.ts
+++ src/app/client/features/features.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexModule } from 'src/app/admin/features/index/index.module';
 import { ProfileModule } from './profile/profile.module';
+import { SettingsModule } from './settings/settings.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, IndexModule, ProfileModule],
+  imports: [
+    CommonModule,
+    IndexModule,
+    ProfileModule,
+    SettingsModule,
+    NotificationsModule,
+  ],
 })
 export class ClientFeaturesModule {}

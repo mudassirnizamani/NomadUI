bac01e2dd58b89f6eda28a796a3286535d58ff45
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-15 02:41:44 -0700

    Made changes in Employee Topbar and Sidebar components

--- src/app/employee/blocks/components/sidebar/sidebar.component.ts
+++ src/app/employee/blocks/components/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SignoutService } from 'src/app/core/services/signout/signout.service';
 
 @Component({
   selector: 'app-employee-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor(private signoutService: SignoutService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signout() {
+    this.signoutService.signout();
   }
-
 }

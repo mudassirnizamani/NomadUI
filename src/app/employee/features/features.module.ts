b663889022bd3485de1ef5407c90311ba555b6b1
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-15 02:43:19 -0700

    Added new Modules in Employee Features Modules

--- src/app/employee/features/features.module.ts
+++ src/app/employee/features/features.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SettingsModule } from './settings/settings.module';
+import { ProfileModule } from './profile/profile.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SettingsModule, NotificationsModule, ProfileModule],
 })
-export class EmployeeFeaturesModule { }
+export class EmployeeFeaturesModule {}

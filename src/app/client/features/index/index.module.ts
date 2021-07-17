8c0647eb72ca8609fc31d17c8d2583af8a3ef5a1
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-17 15:38:00 -0700

    Made changes in Employee INdex feature

--- src/app/client/features/index/index.module.ts
+++ src/app/client/features/index/index.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ProjectsSummaryComponent } from './components/projects-summary/projects-summary.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { IndexComponent } from './index.component';
 
 @NgModule({
-  declarations: [IndexComponent, ProjectsSummaryComponent],
+  declarations: [IndexComponent, ProjectsSummaryComponent, ProjectsComponent],
   imports: [],
 })
 export class IndexModule {}

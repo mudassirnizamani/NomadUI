73f08c9a10cb4e4feb94dba1e042f384fd44143f
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-18 22:45:09 -0700

    MAde some changes in ILead Model

--- src/app/core/models/ILead.interface.ts
+++ src/app/core/models/ILead.interface.ts
@@ -1,3 +1,19 @@
 export interface ILead {
-  
+  id: string;
+  name: string;
+  address: string;
+  position: string;
+  city: string;
+  email: string;
+  province: string;
+  website: string;
+  country: string;
+  phone: string;
+  zipCode: string;
+  defaultLanguage: string;
+  company: string;
+  description: string;
+  status: string;
+  source: string;
+  assignedTo: string;
 }

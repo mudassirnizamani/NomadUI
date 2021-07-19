2b7d972634728417b621f8652693e23decf35c5b
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-18 22:47:08 -0700

    Getting User Data from Store

--- src/app/shared/components/sidebar-user-profile/sidebar-user-profile.component.ts
+++ src/app/shared/components/sidebar-user-profile/sidebar-user-profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { IUser } from 'src/app/core/models/IUser.interface';
 import { UserService } from 'src/app/core/services/user/user.service';
+import { getUser } from 'src/app/store/actions/user.actions ';
+import { UserState } from 'src/app/store/reducers/user/user.reducer';
+import { userSelector } from 'src/app/store/selectors/user/user.selectors';
 
 @Component({
   selector: 'app-sidebar-user-profile',
@@ -8,25 +13,34 @@ import { UserService } from 'src/app/core/services/user/user.service';
 })
 export class SidebarUserProfileComponent implements OnInit {
   isLoading: boolean = false;
-  userData: any;
+  userData: IUser;
+  user$ = this.store.pipe(select(userSelector));
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private store: Store<UserState>
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.userService.getAuthenticatedUser().subscribe(
-      (res: any) => {
-        if (res.succeeded) {
-          this.userData = res.user;
-          this.isLoading = false;
-        } else if (!res.succeeded && res.code == 'ServerError') {
-          this.userData.firstName = 'Error';
-        }
-      },
-      (error: any) => {
-        this.userData.firstName = 'Error';
-        this.isLoading = false;
-      }
-    );
+    // this.userService.getAuthenticatedUser().subscribe(
+    //   (res: any) => {
+    //     if (res.succeeded) {
+    //       this.userData = res.user;
+    //       this.isLoading = false;
+    //     } else if (!res.succeeded && res.code == 'ServerError') {
+    //       this.userData.firstName = 'Error';
+    //     }
+    //   },
+    //   (error: any) => {
+    //     this.userData.firstName = 'Error';
+    //     this.isLoading = false;
+    //   }
+    // );
+    this.store.dispatch(getUser());
+    this.user$.subscribe((data) => {
+      this.userData = data;
+      this.isLoading = false;
+    });
   }
 }

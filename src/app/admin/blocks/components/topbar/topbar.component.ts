e9a3caf3b263f0e6256fc99392da436d222274c3
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-18 22:43:25 -0700

    Getting User Data from Store

--- src/app/admin/blocks/components/topbar/topbar.component.ts
+++ src/app/admin/blocks/components/topbar/topbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { IUser } from 'src/app/core/models/IUser.interface';
 import { SignoutService } from 'src/app/core/services/signout/signout.service';
+import { getUser } from 'src/app/store/actions/user.actions ';
+import { UserState } from 'src/app/store/reducers/user/user.reducer';
+import { userSelector } from 'src/app/store/selectors/user/user.selectors';
 
 // Declaring some javascript functions - Mudasir Ali
 declare const toggle_sidebar_2: any;
@@ -10,10 +15,17 @@ declare const toggle_sidebar_2: any;
   styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent implements OnInit {
-  constructor(private signoutService: SignoutService) {}
+  profilePic: string;
+  user$ = this.store.pipe(select(userSelector));
+
+  constructor(private signoutService: SignoutService, private store: Store<UserState>) {}
 
   ngOnInit(): void {
     toggle_sidebar_2();
+    this.store.dispatch(getUser());
+    this.user$.subscribe((data) => {
+      this.profilePic = data.profilePic;
+    });
   }
 
   signout() {

40985fbccd5c72a687bc1e864e88a136345393a0
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-18 22:44:30 -0700

    Getting User Data from Store

--- src/app/admin/features/index/index.component.ts
+++ src/app/admin/features/index/index.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
 import { IUser } from 'src/app/core/models/IUser.interface';
 import { UserService } from 'src/app/core/services/user/user.service';
+import { getUser } from 'src/app/store/actions/user.actions ';
+import { UserState } from 'src/app/store/reducers/user/user.reducer';
+import { userSelector } from 'src/app/store/selectors/user/user.selectors';
 
 @Component({
   selector: 'app-index',
@@ -10,27 +13,33 @@ import { UserService } from 'src/app/core/services/user/user.service';
   styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
-  user: IUser;
   isLoading: boolean = false;
+  user: IUser;
+  user$ = this.store.pipe(select(userSelector));
 
   constructor(
     private userService: UserService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private store: Store<UserState>
   ) {}
 
   ngOnInit(): void {
-    this.userService.getAuthenticatedUser().subscribe(
-      (res: any) => {
-        if (res.succeeded) {
-          this.user = res.user;
-        } else if (!res.succeeded && res.code == 'ServerError') {
-          this.toastr.error(res.description);
-        }
-      },
-      (error: any) => {
-        this.isLoading = false;
-        this.toastr.error("Server didn't respond", 'Plz try later');
-      }
-    );
+    // this.userService.getAuthenticatedUser().subscribe(
+    //   (res: any) => {
+    //     if (res.succeeded) {
+    //       this.user = res.user;
+    //     } else if (!res.succeeded && res.code == 'ServerError') {
+    //       this.toastr.error(res.description);
+    //     }
+    //   },
+    //   (error: any) => {
+    //     this.isLoading = false;
+    //     this.toastr.error("Server didn't respond", 'Plz try later');
+    //   }
+    // );
+    this.store.dispatch(getUser());
+    this.user$.subscribe((data) => {
+      this.user = data;
+    });
   }
 }

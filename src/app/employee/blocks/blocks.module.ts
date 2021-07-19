41f629a4a544fc6c94a2d287b73505ce62ec579e
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-19 01:33:56 -0700

    Added NzIconModule in Employee Blocks modules

--- src/app/employee/blocks/blocks.module.ts
+++ src/app/employee/blocks/blocks.module.ts
@@ -9,9 +9,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 // Importing Modules - Mudasir Ali
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [IndexLayoutComponent, SidebarComponent, TopbarComponent],
-  imports: [RouterModule, SharedModule, NzDropDownModule],
+  imports: [RouterModule, SharedModule, NzDropDownModule, NzIconModule],
 })
 export class EmployeeBlocksModule {}

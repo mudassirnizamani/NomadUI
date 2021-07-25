e1f0f5108d98120d3d19f859a1d5792c1e8781b9
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-25 05:33:46 -0700

    Made some changes in User Card Compoent in Admin

--- src/app/admin/features/users/components/user-card/user-card.component.ts
+++ src/app/admin/features/users/components/user-card/user-card.component.ts
@@ -4,15 +4,12 @@ import { IUser } from 'src/app/core/models/IUser.interface';
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
 })
 export class UserCardComponent implements OnInit {
+  @Input() userData: IUser;
 
-  @Input() userData: IUser
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

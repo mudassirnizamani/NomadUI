eaca5ed8ef39b66343f2de9726d734ed907ed025
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-07-25 05:12:36 -0700

    Created Notifications Functionality in Admin

--- src/app/admin/features/notifications/notifications.component.ts
+++ src/app/admin/features/notifications/notifications.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { INotification } from 'src/app/core/models/INotification.interface';
+import { getNotifications } from 'src/app/store/actions/notifications/notifcations.actions';
+import { NotificationsState } from 'src/app/store/reducers/notifications/notifcations.reducer';
+import { notificationsSelector } from 'src/app/store/selectors/notifications/notifcations.selectors';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
+  notifications: INotification[];
+  notifications$ = this.store.pipe(select(notificationsSelector));
+  
 
-  constructor() { }
+  constructor(private store: Store<NotificationsState>) {}
 
   ngOnInit(): void {
+    this.store.dispatch(getNotifications());
+    this.notifications$.subscribe((data) => (this.notifications = data));
+    console.log(this.notifications)
   }
-
 }
--- src/app/admin/features/notifications/notifications.module.ts
+++ src/app/admin/features/notifications/notifications.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { NotificationsRoutingModule } from './notifications-routing.module';
 import { NotificationsComponent } from './notifications.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 
 @NgModule({
   declarations: [
-    NotificationsComponent
+    NotificationsComponent,
+    NotificationComponent
   ],
   imports: [
     CommonModule,
